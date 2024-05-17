d6920d65f22a98658eedf60f637df4a39db87b73
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2024-05-17 23:41:39 +0530

    Fix logging service

--- libs/logging/src/logging.middleware.ts
+++ libs/logging/src/logging.middleware.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  NestMiddleware,
-  BadRequestException,
-} from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { LoggingService } from './logging.service';
 import { CreateTokenAccessLogDto } from './dto/create-token-access-log.dto';
@@ -12,10 +8,10 @@ export class TokenAccessLoggingMiddleware implements NestMiddleware {
   constructor(private readonly loggingService: LoggingService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
-    const { key } = req.body;
+    const key = req.headers['x-api-key'] as string;
 
     if (!key) {
-      return next(new BadRequestException('Key is missing'));
+      return next();
     }
 
     res.on('finish', async () => {
